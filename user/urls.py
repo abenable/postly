d82d906b3709ae6b549633a55994f6a4b3ee6806
abenable <ableabenaitwe@gmail.com>
2025-03-07 12:59:55 +0300

    add user authentication and password management URLs

---
+++ user/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+app_name = 'user'
+
+urlpatterns = [
+    # Authentication URLs
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('profile/', views.user_profile, name='profile'),
+    
+    # Password management URLs
+    path('password-change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', views.CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
